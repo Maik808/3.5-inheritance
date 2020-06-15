915f2eff1f501366cd4300416b4cd724ab26f0b1
Mikhail Chebaev <make7650@rambler.ru>
2020-06-15 13:33:00 +0300

    Editing commit

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -17,6 +17,8 @@ class ProductManagerTest {
     private Product productTwo = new Book(2, "The Colour of Magic", 200, "Terry Pratchett");
     private Product productThree = new Smartphone(3, "510", 7000, "Motorolla");
     private Product productFour = new Smartphone(4, "Galaxy S20", 60000, "Samasung");
+    private Product[] notProduct = new Product[0];
+
 
     @BeforeEach
     public void setUp() {
@@ -28,29 +30,39 @@ class ProductManagerTest {
 
     @Test
     void shouldSearchAuthor() {
-        Product[] expected = {new Book(1, "War and Peace", 250, "Leo Tolstoy")};
+        Product[] expected = {productOne};
         Product[] actual = manager.searchBy("Leo Tolstoy");
         assertArrayEquals(expected, actual);
     }
 
     @Test
     void shouldSearchBookName() {
-        Product[] expected =  {new Book(2, "The Colour of Magic", 200, "Terry Pratchett")};
+        Product[] expected =  {productTwo};
         Product[] actual = manager.searchBy("The Colour of Magic");
         assertArrayEquals(expected, actual);
     }
 
     @Test
     void shouldSearchSmarthoneName() {
-        Product[] expected = {new Smartphone(4, "Galaxy S20", 60000, "Samasung")};
+        Product[] expected = {productFour};
         Product[] actual = manager.searchBy("Galaxy S20");
         assertArrayEquals(expected, actual);
     }
 
     @Test
     void shouldSearchManufacturer() {
-        Product[] expected =  {new Smartphone(3, "510", 7000, "Motorolla")};
+        Product[] expected =  {productThree};
         Product[] actual = manager.searchBy("Motorolla");
         assertArrayEquals(expected, actual);
     }
+
+    @Test
+        void shouldNotSearchManufacturer(){
+        Product[] expected = notProduct;
+        Product[] actual = manager.searchBy("Nokia");
+        assertArrayEquals(expected, actual);
+    }
+
+
+
 }
